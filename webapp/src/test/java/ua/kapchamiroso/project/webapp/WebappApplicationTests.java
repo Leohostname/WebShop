357d79a8423c3c4a514ad304048595e377c49d30
Leohostname <l.t.shapoval@gmail.com>
2021-03-08 13:22:18 +0200

    Loading project

---
+++ webapp/src/test/java/ua/kapchamiroso/project/webapp/WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.kapchamiroso.project.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
