d5dccb1f2a6ca29ec5ec47371826c65274073356
Leohostname <l.t.shapoval@gmail.com>
2021-03-11 14:47:23 +0200

    upload

--- webapp/src/main/java/ua/kapchamiroso/project/webapp/WebappApplication.java
+++ webapp/src/main/java/ua/kapchamiroso/project/webapp/WebappApplication.java
@@ -2,13 +2,29 @@ package ua.kapchamiroso.project.webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@Controller
 public class WebappApplication
 {
-
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         SpringApplication.run(WebappApplication.class, args);
     }
 
+    @GetMapping("/hello")
+    @ResponseBody
+    public String sayHello()
+    {
+        return "Du hast";
+    }
+
+    @GetMapping("/mainPage")
+    public String mainPage()
+    {
+        return "index";
+    }
 }
