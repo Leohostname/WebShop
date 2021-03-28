26c9d01635817628036a3dbac2b8382105a40304
Miroso <vaniastronov@gmail.com>
2021-03-28 21:44:15 +0300

    Added test implementation of frontend with fetch method

--- webapp/src/main/java/ua/kapchamiroso/project/webapp/TestController.java
+++ webapp/src/main/java/ua/kapchamiroso/project/webapp/TestController.java
@@ -1,25 +1,29 @@
 package ua.kapchamiroso.project.webapp;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 public class TestController {
     @Autowired
     private MessageRepository messageRepository;
 
     @GetMapping("/helo")
     public String test() {
-        return "Heloworld";
+        return "jsTest";
     }
 
+    @ResponseBody
     @GetMapping("/test")
     public Message getMessage(@RequestParam long id) {
         return messageRepository.findById(id).orElseGet(() -> { throw new RuntimeException("no message"); });
     }
 
+    @ResponseBody
     @PostMapping(value = "/test", consumes = "application/json")
-    public void saveMessage(@RequestBody Message message) {
+    public String saveMessage(@RequestBody Message message) {
         messageRepository.save(message);
+        return "saved successfully";
     }
 }
